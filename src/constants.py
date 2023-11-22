8f716520b51647560431165f3a4cf9e7fd39f426
root <root@DESKTOP-SBMINJV>
2023-11-21 22:52:58 -0500

    feat!: swtich to app commmands instead of text commands

--- src/constants.py
+++ src/constants.py
@@ -5,4 +5,5 @@ if os.environ.get('LOAD_FILE', True):
     load_dotenv(find_dotenv())
 
 DISCORD_TOKEN = os.environ.get("DISCORD_TOKEN", "")
+DISCORD_GUILD = os.environ.get("DISCORD_GUILD", "")
 HIDDEN_ROLES = ["@everyone", "Admin", "Bot", "Bored_Developer_Bot_App"]
--- src/main.py
+++ src/main.py
@@ -1,76 +1,77 @@
-from constants import DISCORD_TOKEN, HIDDEN_ROLES
 import discord
-from discord.ext import commands
+
+from discord import app_commands
 from discord.utils import get
 
+from typing import Optional
+
+from constants import DISCORD_TOKEN, HIDDEN_ROLES, DISCORD_GUILD
+
+MY_GUILD = discord.Object(id=DISCORD_GUILD)
+
+
+class ClientClass(discord.Client):
+    def __init__(self, *, intents: discord.Intents):
+        super().__init__(intents=intents)
+        # A CommandTree is a special type that holds all the application command
+        # state required to make it work. This is a separate class because it
+        # allows all the extra state to be opt-in.
+        # Whenever you want to work with application commands, your tree is used
+        # to store and work with them.
+        # Note: When using commands.Bot instead of discord.Client, the bot will
+        # maintain its own tree instead.
+        self.tree = app_commands.CommandTree(self)
+
+    # In this basic example, we just synchronize the app commands to one guild.
+    # Instead of specifying a guild to every command, we copy over our global commands instead.
+    # By doing so, we don't have to wait up to an hour until they are shown to the end-user.
+    async def setup_hook(self):
+        # This copies the global commands over to your guild.
+        self.tree.copy_global_to(guild=MY_GUILD)
+        await self.tree.sync(guild=MY_GUILD)
+
+
 intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix='!', intents=intents, case_insensitive=True)
+client = ClientClass(intents=intents)
 
-@bot.command(help="Replies with latency of the bot")
-async def ping(ctx):
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user} (ID: {client.user.id})')
+    print('------')
+
+
+@client.tree.command()
+async def hello(interaction: discord.Interaction):
+    """Says hello!"""
+    await interaction.response.send_message(f'Hi, {interaction.user.mention}')
+
+@client.tree.command()
+@app_commands.describe(
+    role_name='Adds or Removes the role for you, if no role is passed will return a list of avaiable roles.'
+)
+async def role(interaction: discord.Interaction, role_name: Optional[str] = None):
     """
-    Sends a message with the bot's latency.
-
-    Parameters:
-    - ctx (discord.ext.commands.Context): The context of the command.
-
-    Returns:
-    - None
+    Adds or removes a role from the specific user calling the command. If no role name is provided, it replies with a list of available roles.
     """
-    await ctx.send(f'pong! - {round(bot.latency, 2)}ms')
-
-@bot.command(help="Add or Remove the role to the specific user calling command, if no role is passed in passes list of roles avaiable")
-async def role(ctx, role_name=None):
-    """
-    Adds or removes a role from the specific user calling the command. If no role name is provided, it sends a list of available roles.
-
-    Parameters:
-    - ctx (discord.ext.commands.Context): The context of the command.
-    - role_name (str, optional): The name of the role to add or remove. Defaults to None.
-
-    Returns:
-    - None
-    """
-    allowed_roles = [role.name for role in ctx.guild.roles if role.name not in HIDDEN_ROLES]
+    allowed_roles = [role.name for role in interaction.guild.roles if role.name not in HIDDEN_ROLES]
     string_roles = "\n".join(allowed_roles)
 
     if role_name is None:
-        await ctx.send(f"Available roles are:\n{string_roles}")
+        await interaction.response.send_message(f"Available roles are:\n{string_roles}")
     else:
         if role_name in allowed_roles:
-            member = ctx.message.author
+            member = interaction.user
             role = get(member.guild.roles, name=role_name)
 
             if member.get_role(role.id) is None:
                 await member.add_roles(role)
-                await ctx.send(f"You have been added to role {role.name}")
+                await interaction.response.send_message(f"You have been added to role {role.name}")
             else:
                 await member.remove_roles(role)
-                await ctx.send(f"You have been removed from role {role.name}")
+                await interaction.response.send_message(f"You have been removed from role {role.name}")
         else:
-            await ctx.send(f"{role_name} is not a valid role please use one of the following:\n{string_roles}")
+            await interaction.response.send_message(f"{role_name} is not a valid role please use one of the following:\n{string_roles}")
 
 
-@bot.command(help="Sync all commands into the discord server this will allow discord to show matching tree of commands")
-async def sync(ctx):
-    """
-    Syncs all the commands into the Discord server, allowing Discord to display a matching tree of commands.
-
-    Example Usage:
-    ```python
-    !sync
-    ```
-    This command can be used in a Discord server where the bot is present. When the command is executed, it will sync all the commands into the server and display a message saying "Command tree synced."
-
-    Parameters:
-    - ctx (context): The context object representing the invocation of the command.
-
-    Returns:
-    - None
-
-    """
-    await bot.tree.sync()
-    await ctx.send('Command tree synced.')
-
-bot.run(DISCORD_TOKEN)
+client.run(DISCORD_TOKEN)
